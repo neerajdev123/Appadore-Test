b87ffa221b1016be57fb86c3e5d7cba5a3791e39
Neeraj Pisharam <neeraj.p@qburst.com>
2024-04-21 10:59:57 +0530

    added view extension

--- app/src/main/java/com/example/myapplication/view/viewholder/ParticipantViewHolder.kt
+++ app/src/main/java/com/example/myapplication/view/viewholder/ParticipantViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.view.viewholder
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.LayoutParticipantViewBinding
+import com.example.myapplication.extensions.show
 import com.example.myapplication.model.Participant
 
 class ParticipantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -11,8 +12,7 @@ class ParticipantViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(participant: Participant){
         binding.imgParticipant.setImageResource(participant.imgRes)
-        //can replace this with extension function
-        binding.txtSpeakerName.visibility = if(participant.isHost) View.VISIBLE else View.GONE
+        binding.txtSpeakerName.show(participant.isHost)
     }
 
 }
